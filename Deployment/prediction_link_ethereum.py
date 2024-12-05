20c855dde3f0b1dd98cfc8d0e35d6aa24c0464c9
ZahrielIsmail <155151831+ZahrielIsmail@users.noreply.github.com>
2024-12-05 18:51:35 +0800

    Update prediction_link_ethereum.py

--- Deployment/prediction_link_ethereum.py
+++ Deployment/prediction_link_ethereum.py
@@ -99,7 +99,7 @@ def extract_coindesk_article_content(url):
 
 
 # Load your model using joblib
-model = joblib.load('decision_tree_gradient_boosting_Ethereum_V2.pkl')
+model = joblib.load('Deployment/decision_tree_gradient_boosting_Ethereum_V2.pkl')
 
 # Placeholder for mean and standard deviation of the original price movements
 mean_price_movement = 10.406232155834717  
